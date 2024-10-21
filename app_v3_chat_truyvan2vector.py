1ae2643cec14cd8ce0cf55e1f38fc1e835aae033
DKMgaming <53993378+DKMgaming@users.noreply.github.com>
2024-10-21 18:54:58 +0700

    Update app_v3_chat_truyvan2vector.py

--- app_v3_chat_truyvan2vector.py
+++ app_v3_chat_truyvan2vector.py
@@ -62,9 +62,9 @@ def save_log_to_google_drive(history, log_filename="user_questions_log.json"):
     log_file_drive.Upload()
     st.success("Log đã được lưu vào Google Drive")
 
-genai_api_key = st.secrets.get("genai_api_key", None)
-if not genai_api_key:
-    st.error("API key cho Gemini không tồn tại.")
+# Thiết lập Gemini API
+genai_api_key = "AIzaSyAfQfOJgGCRxJyDMjr9Kv5XpBGTZX_pASQ"
+genai.configure(api_key=genai_api_key)
 
 
 pc = pinecone.Pinecone(api_key="665d65c5-fb1f-45f9-8bf0-e3ad3d5a93bd")
